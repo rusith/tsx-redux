ca87bd076f4f0db2adc8eef70f7b81a3abb4d3ff
Rusith <rusith@mail.com>
2020-03-07 20:25:57 +0530

    code formatting fixed

--- src/__tests__/action.ts
+++ src/__tests__/action.ts
@@ -11,20 +11,18 @@ describe("createAction", () => {
             .not.toThrow();
     });
 
-
     test("result of an action must have the given type", () => {
         const action = createAction<number>("SomeAction");
         expect(action(0).type).toBe("SomeAction");
     });
 
-
     test("an action should redirect the payload", () => {
         const action = createAction<number>("SomeAction");
         const result = action(1500);
-        
+
         expect(result).toEqual({ payload: 1500, type: "SomeAction"});
     });
-    
+
     test("action should have the given type", () => {
         const action = createAction<number>("SomeAction");
         expect(action.type).toBe("SomeAction");
--- src/action.ts
+++ src/action.ts
@@ -4,12 +4,12 @@ export const createAction = <PayloadType>(type: string) : Action<PayloadType> =>
     if (!type) {
         throw new Error("type is required to create an action");
     }
-    
+
     const action = (payload: PayloadType) => ({
         type,
         payload
     });
     action.type = type;
-    
+
     return action;
 };
